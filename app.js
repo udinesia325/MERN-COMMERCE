f65db807a20d5d7b23a3aff24524c5b78f3e7764
galihapriansyah <galihapriansyah104@gmail.com>
2022-05-16 11:20:53 +0700

    modified

--- app.js
+++ app.js
@@ -2,18 +2,18 @@ const express = require("express");
 const cors = require("cors");
 const app = express();
 const PORT = process.env.PORT || 8000;
-const authRoute = require("./routes/authRoute")
-const productRoute = require("./routes/productRoute")
-require("./routes/productRoute")
-const mongoose = require("mongoose")
+const authRoute = require("./routes/authRoute");
+const productRoute = require("./routes/productRoute");
+require("./routes/productRoute");
+const mongoose = require("mongoose");
 require("dotenv").config();
-const path = require("path")
+const path = require("path");
 //middleware
-app.use(cors())
-app.use(express.json())
+app.use(cors());
+app.use(express.json());
 app.use(express.urlencoded({
   extended: true
-}))
+}));
 app.use(express.static(path.join(__dirname,"public")))
 app.use("/api/auth",authRoute)
 app.use("/api/product", productRoute)
