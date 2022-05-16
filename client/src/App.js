6a425e1790e44c87dfa5431b77c2c55f62ed97eb
galihapriansyah <galihapriansyah104@gmail.com>
2022-05-16 08:24:21 +0700

    Menambahkan Halaman keranjang dan menghapus folder Home

--- client/src/App.js
+++ client/src/App.js
@@ -4,6 +4,7 @@ import { Routes, Route, Link } from "react-router-dom";
 import Utama from "./Component/Utama/Utama";
 import Login from "./Component/Login/Login";
 import Signup from './Signup/Signup';
+import Keranjang from './Component/Keranjang/Keranjang';
 
 function App() {
   return (
@@ -12,6 +13,7 @@ function App() {
         <Route path="/" element={<Utama />} />
         <Route path="/login" element={<Login />} />
         <Route path="/signup" element={<Signup />} />
+        <Route path="/keranjang" element={<Keranjang />} />
       </Routes>
     </div>
   );
