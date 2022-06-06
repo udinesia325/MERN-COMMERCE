d61388f0f6d36f2b6f98e6fd64769171b59bef46
AlbabRiziq <albabriziq@gmail.com>
2022-06-06 23:46:26 +0700

    tes

--- client/src/App.js
+++ client/src/App.js
@@ -1,32 +1,32 @@
-import './App.css';
+import "./App.css";
 import "./App.css";
 import Navbar from "./Component/Navbar/Navbar";
 import Utama from "./Component/Utama/Utama";
 import Login from "./Component/Login/Login";
 import Cart from "./Component/Cart/Cart";
-import Signup from './Component/Signup/Signup';
-import {BrowserRouter as Router, Routes, Route} from "react-router-dom"
-import {loginSuccess} from "./redux/actions/loginActions"
-import CarouselHotSale from "./Component/carouselHotSale/carouselHotSale.jsx"
+import Signup from "./Component/Signup/Signup";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { loginSuccess } from "./redux/actions/loginActions";
+import CarouselHotSale from "./Component/carouselHotSale/carouselHotSale.jsx";
 
 function App() {
   return (
     <div className="App">
-    <Router>
-      <Navbar />
-      <CarouselHotSale />
-      <Routes>
-      <Route path="/" element={<Utama />} exact />
-      <Route path="/login" element={<Login />} exact />
-      <Route path="/signup" element={<Signup />} exact />
-      <Route path="/Cart" element={<Cart />} exact />
+      {/* // <Router>
+    //   <Navbar />
+    //   <CarouselHotSale />
+    //   <Routes>
+    //   <Route path="/" element={<Utama />} exact />
+    //   <Route path="/login" element={<Login />} exact />
+    //   <Route path="/signup" element={<Signup />} exact />
+    //   <Route path="/Cart" element={<Cart />} exact />
      
-      </Routes>
-   {/*   <Utama />
+    //   </Routes> */}
+      {/*   <Utama />
       <Login /> */}
-      {/*<Signup />*/}
-     {/* <Login />*/}
-     </Router>
+      <Signup />
+      {/* <Login />*/}
+      {/* //  </Router> */}
     </div>
   );
 }
