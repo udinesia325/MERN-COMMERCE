b71d245db329c4f6811eb1600d5e0b8f73672b01
udinesia325 <udinesia325@gmail.com>
2022-06-10 13:36:55 +0700

    memperbaiki error jika gagal ambil data dari backend

--- client/src/Component/Utama/Utama.jsx
+++ client/src/Component/Utama/Utama.jsx
@@ -9,12 +9,14 @@ import CarouselHotSale from "../carouselHotSale/carouselHotSale"
 import {axiosInstance} from "../../config"
 function Utama() {
   const [products,setProducts] = useState([])
+  const [error,setError] = useState(false)
   const fetchData = async () => {
     try{
      const response = await axiosInstance.get("product")
      setProducts(response.data)
     }catch(err){
-      console.log(err);
+      setError(true)
+      console.log("Terjadi kesalahan saat mengambil data");
     }
   }
   useEffect(()=>{
@@ -25,7 +27,8 @@ function Utama() {
     <CarouselHotSale />
     <div className="w-[90%] px-4 py-2 mx-auto">
     	<h1 className="font-semibold text-2xl">Produk Populer</h1>
-    	{products.map((product,index)=>(
+    	{error && "Terjadi Kesalahan :(" }
+    	{products.length > 0 && products.map((product,index)=>(
     	<Card key={index} harga={product.harga} nama_barang={product.nama_barang} gambar={"http://localhost:8000"+product.gambar} stock={product.stock} rating={product.rating} keterangan={product.keterangan}/>
     	))}
     </div>
