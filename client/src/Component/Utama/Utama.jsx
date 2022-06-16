0676ce6dc30782a65482b2f50f81d6f1b3c9463a
AndyNotfound <Andy.Notfound@gmail.com>
2022-06-16 17:15:20 +0700

    Positioning of the card

--- client/src/Component/Utama/Utama.jsx
+++ client/src/Component/Utama/Utama.jsx
@@ -27,10 +27,14 @@ function Utama() {
     <CarouselHotSale />
     <div className="w-[90%] px-4 py-2 mx-auto">
     	<h1 className="font-semibold text-2xl">Produk Populer</h1>
-    	{error && "Terjadi Kesalahan :(" }
-    	{products.length > 0 && products.map((product,index)=>(
-    	<Card key={index} harga={product.harga} nama_barang={product.nama_barang} gambar={"http://localhost:8000"+product.gambar} stock={product.stock} rating={product.rating} keterangan={product.keterangan}/>
-    	))}
+      <div className="flex flex-col justify-center items-center gap-y-px
+                      md:flex-row md:gap-x-6 md:justify-start">
+        {error && "Terjadi Kesalahan :(" }
+        {products.length > 0 && products.map((product,index)=>(
+        <Card key={index} harga={product.harga} nama_barang={product.nama_barang} gambar={"http://localhost:8000"+product.gambar} stock={product.stock} rating={product.rating} keterangan={product.keterangan}/>
+        ))}
+      </div>
+    	
     </div>
    </>
   );
