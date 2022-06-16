bea8ac9ddb54dc9f97c13d97af71d491a6b95298
udinesia325 <udinesia325@gmail.com>
2022-06-16 16:48:00 +0700

    menambahkan login dan logout pada menumobile

--- client/src/Component/Navbar/menuMobile.jsx
+++ client/src/Component/Navbar/menuMobile.jsx
@@ -1,8 +1,11 @@
 import dummyUser from "../img/dummyUser.png";
-import {useSelector} from "react-redux"
+import {useSelector, useDispatch} from "react-redux"
+import {Link} from "react-router-dom"
+import {logout} from "../../redux/actions/loginActions"
+
 function MenuMobile() {
   const loginState = useSelector(state => state.login)
-
+ const dispatch = useDispatch()
   return (
     <div className="h-screen w-72 float-right bg-blue-50 z-50 p-5 md:m-3 md:rounded-2xl shadow-xl ">
       <div className="flex bg-blue-100 p-5 rounded-lg items-center shadow-md">
@@ -17,7 +20,12 @@ function MenuMobile() {
       <h1 className="font-bold text-lg  mt-5 text-gray-700">
         Riwayat Aktivitas
       </h1>
-      <div></div>
+      
+      {
+        loginState.isLogin ? 
+      <button onClick={()=>dispatch(logout())} className="h-8 w-1/2 bg-red-400 text-white font-semibold block mx-auto">Logout</button> :
+      <Link to="/login" className="h-8 w-1/2 bg-steal-600 text-white font-semibold text-center">Login</Link>
+      }
     </div>
   );
 }
