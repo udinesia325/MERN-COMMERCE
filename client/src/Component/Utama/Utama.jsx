38b7418dc07c3cd051f76dd35121203cbeb63b64
AndyNotfound <Andy.Notfound@gmail.com>
2022-06-17 12:18:46 +0700

    gap

--- client/src/Component/Utama/Utama.jsx
+++ client/src/Component/Utama/Utama.jsx
@@ -27,7 +27,7 @@ function Utama() {
     <CarouselHotSale />
     <div className="w-[90%] px-4 py-2 mx-auto">
     	<h1 className="font-poppins font-semibold text-2xl mt-3 mb-4">Popular Product</h1>
-      <div className="flex flex-col justify-center items-center gap-y-px
+      <div className="flex flex-col justify-center items-center gap-4
                       md:flex-row md:gap-x-6 md:justify-start">
         {error && "Terjadi Kesalahan :(" }
         {products.length > 0 && products.map((product,index)=>(
